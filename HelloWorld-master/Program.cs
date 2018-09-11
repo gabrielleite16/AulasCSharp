22b7642bfe0bd8f36213ba5a911ec73386b590b7
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-09-11 20:28:49 -0300

    Add files via upload

---
+++ HelloWorld-master/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula04092018
+{
+    class Program
+    {
+
+        static int Soma(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa2 = new Pessoa("14523750629", "Gabriel Leite");
+
+            Console.WriteLine(pessoa2.NomeCPF());
+
+            int resultado = Soma(12, 15); 
+
+            Console.WriteLine("\n\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
