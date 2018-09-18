508620339adc77ca59b2e5892222b7d1bdca5806
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-09-18 20:33:32 -0300

    Add files via upload

---
+++ ExercicioiLang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaPoupanca
+{
+    class ContaPoupanca
+    {
+        private static double taxaDeJurosAnual;
+        private double saldoPoupanca;
+        
+        public double CalcularJuromensal()
+        {
+            double jurosInterno;
+            jurosInterno =  (this.saldoPoupanca * taxaDeJurosAnual) / 12;
+
+            saldoPoupanca = jurosInterno;
+
+            return saldoPoupanca;
+        }
+
+        public double AlterarTaxaDeJuros()
+        {
+            double taxaVariavel;
+            double taxaFinal;
+
+            taxaFinal = taxaDeJurosAnual * taxaVariavel;
+
+            taxaDeJurosAnual = taxaFinal;
+
+            return taxaDeJurosAnual;
+        }
+    }
+}
